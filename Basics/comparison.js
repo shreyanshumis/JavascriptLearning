3c584c82bc1ee5b1c73f918850c2d130ab2e927f
Shreyanshu <93574203+shreyanshumis@users.noreply.github.com>
2024-07-27 22:29:01 +0000

    Modifications to comparison.js

--- Basics/comparison.js
+++ Basics/comparison.js
@@ -12,3 +12,7 @@ console.log(null == 0); //res - false
 console.log(null >= 0 ); //res - true
 //The reason is that the equality check == and comparisons > < >= <= work differently. 
 //comparisons convert null into a number, treating it as 0. That's why 3rd null >= 0 is true and 1st one is false
+
+// === strict check -  it checks the datatypes as well. The below examples will be treated differently.
+console.log("2" == 2);
+console.log("2" === 2);
