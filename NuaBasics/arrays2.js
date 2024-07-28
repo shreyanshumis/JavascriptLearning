bce05f6fbdb91431d39c88465911e553c9adb803
Shreyanshu <93574203+shreyanshumis@users.noreply.github.com>
2024-07-28 20:59:10 +0000

    Later commits to arrays2

--- NuaBasics/arrays2.js
+++ NuaBasics/arrays2.js
@@ -23,3 +23,8 @@ const fixedArr = newArr.flat(Infinity) //returns a new array with all sub array 
 //if u give something like Array.from({name: "Shrey"})
 //It returns an empty array since it is NOT able to decide which one to make an array out of, key or value
 
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
