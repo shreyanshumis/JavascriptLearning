4985beace2ea784382a3788f0f968e2044e56966
Shreyanshu <93574203+shreyanshumis@users.noreply.github.com>
2024-07-28 20:57:34 +0000

    Arrays 2

---
+++ NuaBasics/arrays2.js
@@ -0,0 +1,25 @@
+const progLangs = ["Python", "Java", "C++"]
+const humanLangs = ["Odia","English","Japanese"]
+
+progLangs.push(humanLangs) //it adds the array itself as a single element into the other array instead of adding the elements separately.
+
+// console.log(progLangs)
+// console.log(progLangs[3][1])
+
+const newLang = progLangs.concat(humanLangs) //combines 2 or more arrays and returns a new array
+// console.log(newLang)
+
+const allNewLangs = [...progLangs,...humanLangs] //all the elements are split and not like the first case
+// console.log(allNewLangs)
+
+const newArr = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const fixedArr = newArr.flat(Infinity) //returns a new array with all sub array elements
+// console.log(fixedArr)
+
+// Array.isArray("Shrey")  --> false
+// Array.from("Shrey")  --> converts it into an array(S,h,r,e,y)
+
+//if u give something like Array.from({name: "Shrey"})
+//It returns an empty array since it is NOT able to decide which one to make an array out of, key or value
+
