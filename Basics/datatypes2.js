569cb00dab6c3e54ccbb80b8cef5db1779cb7814
Shreyanshu <93574203+shreyanshumis@users.noreply.github.com>
2024-07-28 04:08:24 +0000

    stack and heap memory added to the existing code

--- Basics/datatypes2.js
+++ Basics/datatypes2.js
@@ -32,3 +32,32 @@ let myObj = {
 const myFunc = function(){
     console.log("Hello world");
 } //function
+
+//---------MEMORY----------
+//2 types - stack and heap
+
+//Primitive type - Stack memory(call by value)
+//Non Primitive type - heap memory(call by reference)
+
+//Call by Value :-
+
+let myName = "shrey"
+let newName = myName
+console.log(newName);
+newName = "anshu"
+//newName's value changed but myName's didn't cuz it's copy was given to newName and not the reference itself.
+
+//Call by Reference :-
+
+let user = {
+    email:"user@hey.com",
+    upi: "user@payhey"
+}
+
+let user2 = user
+//This time you're getting the reference and not the copy of the same, hence the value in the original will change if u make changes in the 2nd one(user2)
+
+user2.email = "anshu@hey.com"
+
+console.log(user);
+console.log(user2);
