a28918b4561a2da76a1dde1652b90fed4dbd761f
Shreyanshu <93574203+shreyanshumis@users.noreply.github.com>
2024-07-31 17:29:23 +0000

    foreach

--- Iterations/foreach.js
+++ Iterations/foreach.js
@@ -1,2 +1,42 @@
 const coding = ["js", "ruby", "java", "py", "cpp"]
 
+coding.forEach( function (val) {
+    console.log(val);
+} ) 
+//since it's callback function, it doesnt have name ^
+//it will automatically execute all values
+
+//using arrow function
+coding.forEach( (val) => {
+    console.log(val)
+})
+
+//another way
+
+function printMe(item){
+    console.log(item);
+}
+
+coding.forEach(printMe) //gave func reference.
+
+//a scenario - array of objects
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName); //took a value out of array of objects
+} )
+
